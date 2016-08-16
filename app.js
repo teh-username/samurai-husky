b111553f37c8642efb7e983a59e1816475de2cb0
LA Roberto <louise.roberto.1337@gmail.com>
2016-08-16 14:24:04 +0800

    move dev logging when not in production mode

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -35,6 +34,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.use(logger('dev'));
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
