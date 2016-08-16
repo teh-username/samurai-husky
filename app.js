ed9e80f39b44a0544aa099945121169afa562ea5
LA Roberto <louise.roberto.1337@gmail.com>
2016-08-16 15:33:21 +0800

    add favicon, add security stuffs, removed unused modules

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+var helmet = require('helmet')
 
 var app = express();
 
@@ -13,11 +11,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// helmet setup
+app.use(helmet());
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -34,7 +34,6 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(logger('dev'));
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
